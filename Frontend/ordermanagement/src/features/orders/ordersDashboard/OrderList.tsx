a3e56c9bbf376ca2b22cd04593db20f17da4282c
alfredo.magpantay <magpanta.ext@medgate.ch>
2023-08-21 11:19:11 +0800

    End of section 3

---
+++ Frontend/ordermanagement/src/features/orders/ordersDashboard/OrderList.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useMemo } from 'react';
+import { Customer, Order } from '../../../graphql/generated/schema';
+import { AgGridReact } from 'ag-grid-react';
+
+interface OrderListProps {
+    orders: Order[];
+}
+
+export default function OrderList({ orders }: OrderListProps) {
+    const [columnDefs] = useState([
+        {
+            field: 'id',
+            width: 50,
+            suppressSizeToFit: true,
+        },
+        {
+            field: 'customer',
+            cellRenderer: function (params: any) {
+                const customer = params.value as Customer;
+                return customer.firstName + ' ' + customer.lastName;
+            }
+        },
+        {
+            field: 'orderDate'
+        },
+        { field: 'status' }
+    ]);
+
+    const defaultColDef = useMemo(() => ({
+        sortable: true,
+        filter: true,
+        resizable: true
+    }), []);
+
+    return (
+        <div className='ag-theme-alpine' style={{ height: 500, width: '100%' }}>
+            <AgGridReact
+                rowData={orders}
+                columnDefs={columnDefs}
+                defaultColDef={defaultColDef}
+            />
+        </div>
+    );
+}
