d5f0d44f89474878eaae9fbb4ff2047636cde700
alfredo.magpantay <magpanta.ext@medgate.ch>
2023-08-22 11:02:11 +0800

    Creating SQLite db and migrations

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContextFactory<OMAContext>(options => {
-    options.UseInMemoryDatabase("InMemoryDb");
+    options.UseSqlite(builder.Configuration["ConnectionStrings:DefaultConnection"]);
 });
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
@@ -38,5 +38,16 @@ app.UseCors(AllowSpecificOrigins);
 app.MapGraphQL();
 
 app.UseGraphQLVoyager("/graphql-voyager", new VoyagerOptions { GraphQLEndPoint = "/graphql" });
+
+// Migrate Database
+try {
+    var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<OMAContext>();
+    context.Database.Migrate();
+}
+catch (Exception ex) {
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
